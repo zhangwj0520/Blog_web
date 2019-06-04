c8030d3141cc024b5002c5d7a28d2cd1fd63c3a2
zhangweijie <576579227@qq.com>
2019-06-04 15:08:58 +0800

    fetch

--- src/layouts/BasicLayout/Banner/index.js
+++ src/layouts/BasicLayout/Banner/index.js
@@ -1,7 +1,7 @@
 /**
  * @LastEditors: zhang weijie
  * @Date: 2019-05-29 10:00:58
- * @LastEditTime: 2019-06-04 14:46:09
+ * @LastEditTime: 2019-06-04 15:07:53
  * @Description:
  **/
 import React, { useState, useEffect } from 'react';
@@ -10,16 +10,13 @@ import { Col, Row } from 'antd';
 import Banner from './banner';
 import bordImage from '../../../assets/image/bord.jpg';
 import styles from './style.module.less';
-import { HookGet, Fetch } from '../../../common/fetch';
-import { useAbortableFetch } from '../../../common/hooks';
+import { Fetch } from '../../../common/fetch';
 
 const str = '因为没有丰富阅历和经验！闲下来时多看看书，书本里的故事总有我要学的人生';
 
 const HomeBanner = () => {
     const [sentence, setSentence] = useState(str);
-
-    const [fetchData, result] = Fetch('https://v1.hitokoto.cn/');
-    // const [fetchData, result] = HookGet('https://v1.hitokoto.cn/');
+    const [fetchData, data, total, result] = Fetch('https://v1.hitokoto.cn/');
 
     /*  如果想执行只运行一次的 effect（仅在组件挂载和卸载时执行），
     可以传递一个空数组（[]）作为第二个参数。
@@ -27,19 +24,21 @@ const HomeBanner = () => {
     所以它永远都不需要重复执行。这并不属于特殊情况 —— 它依然遵循输入数组的工作方式。 */
 
     useEffect(() => {
-        //这个连续执行了6次为了测试abort功能,打开浏览器查看
+        //这个连续执行了3次为了测试abort功能,打开浏览器查看
         fetchData({ c: 'f' });
         fetchData({ c: 'f' });
         fetchData({ c: 'f' });
-        // fetchData({ c: 'f' });
-        // fetchData({ c: 'f' });
-        // fetchData({ c: 'f' });
-    }, []);
+    }, [fetchData]);
 
     useEffect(() => {
+        //这是为了显示abost
         console.log(result);
-        result.hitokoto && setSentence(result.hitokoto);
     }, [result]);
+
+    useEffect(() => {
+        console.log(data);
+        setSentence(data.hitokoto);
+    }, [data]);
 
     return (
         <div className={styles.container}>
